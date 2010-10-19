6f0a3734cb6b47cc2d0a4ef68e9975efb1e60d47
Ryan Nielson <rnielson@west-inc.com>
2010-10-19 00:00:00 +0000

    version 2.0

--- R/bbmm.summary.R
+++ R/bbmm.summary.R
@@ -1,11 +1,11 @@
 bbmm.summary <-
 function(x){
 #
-#   print method for patch occupancy objects
+#   print method for BBMM objects
 #
     cat("\nBrownian motion variance : ", x[[1]], fill=TRUE) 
-    cat("Size of Grid : ", length(x$x), "Cells", fill=TRUE)
-    cat( "Grid Cell Size : ", abs(x$x[1]-x$x[2]), fill=TRUE)
+    cat("Size of grid : ", length(x$x), "cells", fill=TRUE)
+    cat( "Grid cell size : ", abs(x$x[1]-x$x[2]), fill=TRUE)
 
 }
 
--- R/brownian.bridge.R
+++ R/brownian.bridge.R
@@ -2,20 +2,25 @@ brownian.bridge <-
 function(x, y, time.lag, location.error, area.grid=NULL, cell.size=NULL, time.step=10){
 
     # Input: 
-    #   x, y = vectors of coordinates of ordered animal locations, in UTMs
-    #   time.lag = vector of time differences (in minutes) between successive 
-    #           locations. length(time.lag) = length(x) - 1. 
-    #   location.error = either a mean location error or a vector of errors 
-    #                   (1 for each location)
-    #   area.grid = matrix or data frame of X and Y coords for brownian bridge. 
-    #           If missing, defaults to minimum/maximum x/y minus/plus 10% of the 
+    #   x, y = Vectors of coordinates of ordered animal locations, in UTMs.
+    #   time.lag = Vector of time differences (in minutes) between successive 
+    #           locations. Length(time.lag) = length(x) - 1. 
+    #   location.error = Either the standarad deviation of normally distributed 
+	#			location error or a vector of SDs (1 for each location).
+    #   area.grid = Matrix or data frame of "x" and "y" coords for brownian bridge. 
+    #           If missing, defaults to minimum/maximum x/y minus/plus 1/2 sd of the  
     #           range of x/y. If area.grid is provided, cell sizes must be square and uniform. 
-    #   cell.size = cell size for grid, if grid not provided
-    #   time.step = 
+    #   cell.size = Cell size for grid, if grid not provided.
+    #   time.step = The Brownian bridge probability density function 
+	#			must be integrated to find the fraction of time spent in each region. While the 
+	#			probability density function cannot be integrated, it can be approximated by 
+	#			discretizing time into arbitrarily small intervals of time.step. The default 
+	#			is 10 units (same as time.lag). Longer time.step speeds up estimation, 
+	#			but reduces precision.
     # Output: 
-    #   UD = list with estimated Brownian.Motion.Variance, X, Y, and Z, 
-    #       where X and Y are grid center point coordinates and Z is the estimated
-    #       probability of use with sum(Z) = 1.0.
+    #   UD = List with estimated Brownian.Motion.Variance, "x", "y", and "z", 
+    #       where x and y are grid center point coordinates and z is the estimated
+    #       probability of use with sum(z) = 1.0.
     
     if(is.null(x) | is.null(y) | (length(x) != length(y))) {
         stop("data is missing or unequal number of x and y coordinates")
@@ -25,41 +30,41 @@ function(x, y, time.lag, location.error, area.grid=NULL, cell.size=NULL, time.st
         stop("'area.grid' or 'cell.size' must be specified")
     }
     if(!is.null(area.grid) & is.null(cell.size)){
-        cell.size = abs(area.grid[1,1] - area.grid[2,1]) 
+        cell.size <- abs(area.grid[1,1] - area.grid[2,1]) 
     }
     if(is.null(area.grid) & !is.null(cell.size)){
-        range.x = range(x)
-        range.y = range(y)
-        min.grid.x = round(range.x[1] - 1*sd(x))
-        max.grid.x = round(range.x[2] + 1*sd(x))
-        min.grid.y = round(range.y[1] - 1*sd(y))
-        max.grid.y = round(range.y[2] + 1*sd(y))
-        X=seq(min.grid.x, max.grid.x, cell.size) 
-        Y=seq(min.grid.y, max.grid.y, cell.size)
-        area.grid = merge(X, Y)
+        range.x <- range(x)
+        range.y <- range(y)
+        min.grid.x <- round(range.x[1] - 1*sd(x))
+        max.grid.x <- round(range.x[2] + 1*sd(x))
+        min.grid.y <- round(range.y[1] - 1*sd(y))
+        max.grid.y <- round(range.y[2] + 1*sd(y))
+        x. <- seq(min.grid.x, max.grid.x, cell.size) 
+        y. <- seq(min.grid.y, max.grid.y, cell.size)
+        area.grid <- merge(x., y.)
     }
     
     if(length(location.error) == 1){
-        location.error = rep(location.error, length(x))
+        location.error <- rep(location.error, length(x))
     }
     
-    n.locs = length(x)
+    n.locs <- length(x)
 
-    BMvar = brownian.motion.variance(n.locs, time.lag, location.error, x, y)
-    BMvar = rep(BMvar, times=length(x))
+    BMvar <- brownian.motion.variance(n.locs, time.lag, location.error, x, y)
+    BMvar <- rep(BMvar, times=length(x))
 
     # Use 10 units (generally minutes) as default.
-    if(is.null(time.step)) time.step = 10
+    if(is.null(time.step)) time.step <- 10
 
-    grid.size = nrow(area.grid)
-    probability = rep(0, grid.size)
-    T.Total = sum(time.lag)
+    grid.size <- nrow(area.grid)
+    probability <- rep(0, grid.size)
+    T.Total <- sum(time.lag)
 
-    bbmm = vector("list", 4)
-    names(bbmm) = c("Brownian motion variance", "x", "y", "probability")
-    class(bbmm) = "bbmm"
+    bbmm <- vector("list", 4)
+    names(bbmm) <- c("Brownian motion variance", "x", "y", "probability")
+    class(bbmm) <- "bbmm"
     
-    #dyn.load("BBMM.dll")
+    #dyn.load("BBMM.dll")	# comment out before package creation
 
     ans <- .Fortran("BBMM",
                     as.integer(n.locs), 
@@ -70,19 +75,19 @@ function(x, y, time.lag, location.error, area.grid=NULL, cell.size=NULL, time.st
                     as.double(y), 
                     as.double(BMvar), 
                     as.double(location.error), 
-                    as.double(area.grid$x), 
-                    as.double(area.grid$y), 
+                    as.double(area.grid[,1]), 
+                    as.double(area.grid[,2]), 
                     as.double(time.step), 
                     as.double(probability),
                     PACKAGE="BBMM")
     
-    bbmm[[4]] = ans[[12]]    
+    bbmm[[4]] <- ans[[12]]    
 
-    #dyn.unload("BBMM.dll")
+    #dyn.unload("BBMM.dll")		# comment out before package creation
 
-    bbmm[[1]] = BMvar[1]
-    bbmm[[2]] = area.grid$x
-    bbmm[[3]] = area.grid$y
+    bbmm[[1]] <- BMvar[1]
+    bbmm[[2]] <- area.grid[,1]
+    bbmm[[3]] <- area.grid[,2]
 
     return(bbmm)
 
